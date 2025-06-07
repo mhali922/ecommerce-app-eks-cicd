e9e7e1bc6f3f216336dd2243c2634d82fdba414e
mhali922 <56911307+mhali922@users.noreply.github.com>
2025-06-07 15:10:22 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,8 @@
-@#
+import axios from "axios";
+
+const API_BASE_URL = process.env.REACT_APP_PRODUCT_API_URL || "http://localhost:3001";
+
+export const fetchProducts = async () => {
+  const response = await axios.get(`${API_BASE_URL}/products`);
+  return response.data;
+};
