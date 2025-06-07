764d9fceea65dff3dbd48d02a496cd6596217b9e
mhali922 <56911307+mhali922@users.noreply.github.com>
2025-06-07 15:07:38 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,20 @@
-#will put code
+import React, { useEffect, useState } from "react";
+import ProductList from "./components/ProductList";
+import { fetchProducts } from "./api";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts().then(setProducts).catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      <h1>E-Commerce Store</h1>
+      <ProductList products={products} />
+    </div>
+  );
+};
+
+export default App;
